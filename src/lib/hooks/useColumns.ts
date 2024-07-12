bc403e050e4c3e1694717042e972ea146697e84a
tetrica <fluiz.cba@gmail.com>
2024-07-12 11:18:06 -0400

    feat: funcao de estrutura da tabela

---
+++ src/lib/hooks/useColumns.ts
@@ -0,0 +1,74 @@
+import { useMemo } from "react";
+import { Key, isNestedGroups } from "../typings";
+import { ColumnDef } from "@tanstack/react-table";
+
+function useColumns<T extends Record<Key, any>>(
+  groupedData: Record<string, T[]> | Record<string, Record<string, T[]>>,
+  somarPor: Key,
+  visualizarPor: Key,
+  detalharPor?: Key
+) {
+  return useMemo(() => {
+    if (!somarPor || !visualizarPor) {
+      return [];
+    }
+
+    if (isNestedGroups(groupedData, detalharPor)) {
+      const keys = new Set<string>(
+        Object.values(groupedData).flatMap((v) => Object.keys(v))
+      );
+
+      const columns: ColumnDef<T, unknown>[] = [
+        {
+          accessorKey: "xAxis",
+          header: visualizarPor as string,
+          cell: ({ row }) => row.original?.["xAxis"] ?? "",
+          footer: () => {
+            return "Total";
+          },
+        },
+      ];
+
+      keys.forEach((key) => {
+        columns.push({
+          accessorKey: key,
+          header: key,
+          cell: ({ row }) => row.original?.[key] ?? "",
+          footer: ({ table }) => {
+            return table
+              .getFilteredRowModel()
+              .rows?.reduce((sum, row) => sum + (row.original?.[key] ?? 0), 0);
+          },
+        });
+      });
+
+      return columns;
+    }
+
+    return [
+      {
+        accessorKey: "xAxis",
+        header: visualizarPor,
+        cell: ({ row }) => row.original?.["xAxis"] ?? "",
+        footer: () => {
+          return "Total";
+        },
+      },
+      {
+        accessorKey: visualizarPor,
+        header: somarPor,
+        cell: ({ row }) => row.original?.[visualizarPor] ?? "",
+        footer: ({ table }) => {
+          return table
+            .getFilteredRowModel()
+            .rows?.reduce(
+              (sum, row) => sum + (row.original?.[visualizarPor] ?? 0),
+              0
+            );
+        },
+      },
+    ] as ColumnDef<T, unknown>[];
+  }, [groupedData, detalharPor, somarPor, visualizarPor]);
+}
+
+export default useColumns;
