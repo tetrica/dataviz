928385045edd873b4bd4e55eadc78a742cd7cefc
tetrica <fluiz.cba@gmail.com>
2024-07-11 22:22:56 -0400

    feat: define rotas da aplicacao

--- src/components/routes.tsx
+++ src/components/routes.tsx
@@ -1,10 +1,12 @@
 import { Route, Switch, Redirect } from "wouter";
+import DataSourceSelect from "./data-source-select";
+import Visualizer from "./visualizer";
 
 function Routes() {
   return (
     <Switch>
-      <Route path="/" component={() => <h1>Home</h1>} />
-      <Route path="/chart" component={() => <h1>Chart</h1>} />
+      <Route path="/" component={DataSourceSelect} />
+      <Route path="/visualizer" component={Visualizer} />
       <Route component={() => <Redirect to="/" />} />
     </Switch>
   );
